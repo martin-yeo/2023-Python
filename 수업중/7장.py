0fa2b1fa190ab6a1a48c9222422c403bcc4925c5
martin-yeo <sangsooyeo@gmail.com>
2023-05-16 11:38:58 +0900

    7장+8장

--- 수업중/7장.py
+++ 수업중/7장.py
@@ -62,28 +62,25 @@
 
 #### 데크 (deque)
 # from collections import deque
-<<<<<<< HEAD
-import collections
 
-word = input("문자열을 입력하세요 : ")
-print('문자열 word : ', word)
-d = collections.deque(word)
-print('데크 d : ', d)
-d.rotate(-2)
-print(d)
-print(d.pop())
-print(d.pop())
-# print(d.pop(0))
-print(d.popleft())
-print(d)
-d.append('카')
-d.append('타')
-d.appendleft('하')
-print(d)
-rd = deque(reversed(d))
-print(d)
-print(rd)
-=======
+# word = input("문자열을 입력하세요 : ")
+# print('문자열 word : ', word)
+# d = deque(word)
+# print('데크 d : ', d)
+# d.rotate(-2)
+# print(d)
+# print(d.pop())
+# print(d.pop())
+# # print(d.pop(0))
+# print(d.popleft())
+# print(d)
+# d.append('카')
+# d.append('타')
+# d.appendleft('하')
+# print(d)
+# rd = deque(reversed(d))
+# print(d)
+# print(rd)
 
 # word = input("문자열을 입력하세요 : ")
 # d = deque(word)
@@ -102,4 +99,10 @@ print(rd)
 # rd = deque(reversed(d))
 # print(d)
 # print(rd)
->>>>>>> 9a0b583d65704967c88eb880950d6e1b69e83346
+
+from collections import Counter
+
+text = '오늘 오후에는 오전보다 수업을 많이 하지 않습니다.'.split()
+print(text)
+counterText = Counter(text)
+print(counterText)
