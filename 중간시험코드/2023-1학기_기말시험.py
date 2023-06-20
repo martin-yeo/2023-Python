e3cbc86c9726d2a49e1fd4f3b6be49f5438a2501
martin-yeo <sangsooyeo@gmail.com>
2023-06-20 10:43:59 +0900

    과제수행결과 업데이트

---
+++ 중간시험코드/2023-1학기_기말시험.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = s
+    answer = answer.upper()
+    return answer
+
+
+
+
+print( solution("ThisIsString") )
+print( solution("ABCDEFG") )
+print( solution("aBcDeFgHiJkLmNopqrsTU") )
